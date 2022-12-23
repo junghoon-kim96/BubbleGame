9a14210c14b18df9eca84c39c8510e025778d73d
junghoonkim96 <0527wj@gmail.com>
2022-12-23 22:53:19 +0900

    윈도우 빌더 생성 , JFrame 설정

---
+++ src/main/java/com/exemple/bubble/App.java
@@ -0,0 +1,4 @@
+package com.exemple.bubble;
+
+public class App {
+}
--- src/main/java/com/exemple/bubble/BubbleApplication.java
+++ src/main/java/com/exemple/bubble/BubbleApplication.java
@@ -10,6 +10,9 @@ public class BubbleApplication extends JFrame {
 
     public BubbleApplication (){
         setSize(1000, 640);
+        setLayout(null); // absoulte 레이아웃 (자유롭게 그림을 그릴 수 있다)
+        setLocationRelativeTo(null); // JFrame 가운데 배치
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
